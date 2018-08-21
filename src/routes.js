c8dd811a3da274995e555d6232131103864879e4
Luka Giorgadze <6573695+LukaGiorgadze@users.noreply.github.com>
2018-08-21 13:59:15 +0400

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,15 @@
 // Libraries
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, Push } from 'react-router-dom';
-//
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
 // // Components
-// import Home from 'components/home';
-// import About from 'components/about';
-// import Contact from 'components/contact';
-//
-// // Routes
-// export const Routes = () => (
-//   <BrowserRouter>
-//     <div>
-//       <ul>
-//         <li>
-//           <Link to="/">Home</Link>
-//         </li>
-//         <li>
-//           <Link to="/about">About</Link>
-//         </li>
-//         <li>
-//           <Link to="/contact">Contact</Link>
-//         </li>
-//       </ul>
-//       <Route exact path="/" component={Home} />
-//       <Route path="/about" component={About} />
-//       <Route path="/contact" component={Contact} />
-//     </div>
-//   </BrowserRouter>
-// );
+import Wrapper from 'components/index';
+
+// Routes
+export const Routes = () => (
+  <BrowserRouter>
+    <div>
+      <Wrapper />
+    </div>
+  </BrowserRouter>
+);

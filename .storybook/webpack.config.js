57ecaa0a666aef1d7e561c593afdcb0d41fd1ca7
Luka Giorgadze <6573695+LukaGiorgadze@users.noreply.github.com>
2018-08-21 13:56:09 +0400

    Update webpack.config.js

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,28 @@
+const Path = require('path');
 const common = require('../webpack.common.js');
+
+const publicDir = 'public';
 
 module.exports = {
   resolve: {
     extensions: common.resolve.extensions,
+    alias: {
+      public: Path.resolve(__dirname, `../${publicDir}/`),
+      src: Path.resolve(__dirname, `../src/`),
+      components: Path.resolve(__dirname, '../src/components/'),
+      ui: Path.resolve(__dirname, '../src/ui/'),
+      actions: Path.resolve(__dirname, '../src/actions/'),
+      reducers: Path.resolve(__dirname, '../src/reducers/'),
+      sagas: Path.resolve(__dirname, '../src/sagas/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: ['url-loader', 'img-loader'],
+        include: Path.resolve(__dirname, `../`)
+      }
+    ],
   },
 };
